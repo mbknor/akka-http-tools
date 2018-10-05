5810dc727157dfb485c5247aaa02edfb87d49f4d
Morten Kjetland <mbk@kjetland.com>
2018-10-05 10:57:33 +0200

    Added JsonTestHelper

---
+++ akka-http-tools-core/src/main/scala/com/kjetland/ahtc/testUtils/JsonTestHelper.scala
@@ -0,0 +1,43 @@
+package com.kjetland.ahtc.testUtils
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ArrayNode
+
+import scala.reflect.ClassTag
+import scala.collection.JavaConverters._
+
+trait JsonTestHelper {
+
+  def createMapperFactory:ObjectMapper
+
+  lazy val objectMapper:ObjectMapper = createMapperFactory
+  lazy val prettyObjectMapper = objectMapper.writerWithDefaultPrettyPrinter()
+
+  def toJson(o:Any):String = {
+    objectMapper.writeValueAsString(o)
+  }
+
+  def toJsonPretty(o:Any):String = {
+    prettyObjectMapper.writeValueAsString(o)
+  }
+
+
+  def convertFromJson[T:ClassTag](json:String):T = {
+    def ctag = implicitly[reflect.ClassTag[T]]
+    objectMapper.readValue(json, ctag.runtimeClass.asInstanceOf[Class[T]])
+  }
+
+  def convertFromJsonList[T:ClassTag](json:String):List[T] = {
+    def ctag = implicitly[reflect.ClassTag[T]]
+    val clazz = ctag.runtimeClass.asInstanceOf[Class[T]]
+    val tree = objectMapper.readTree(json).asInstanceOf[ArrayNode]
+
+    var list = List[T]()
+    tree.elements().asScala.foreach { e =>
+      list = list :+ objectMapper.treeToValue(e, clazz)
+    }
+
+    list
+  }
+
+}
